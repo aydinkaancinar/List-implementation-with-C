a0a2f90901a384bf0f6bbc9de9690d7f3b7017d0
Aydin Kaan Cinar <43555453+aydinkaancinar@users.noreply.github.com>
2020-02-18 14:04:47 -0500

    file added

---
+++ linkedlist.c
@@ -0,0 +1,153 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node {
+    int val;
+    struct node *next;
+} Node;
+
+typedef struct list {
+    Node *first;
+} List;
+
+List new_list(){
+    List list;
+    list.first=NULL;
+    return list;
+}
+
+void insert(List *l, int val){
+    if(l->first == NULL){
+        l->first = malloc(sizeof(Node));
+        l->first->val = val;
+        l->first->next = NULL;
+    }
+    else{
+        Node *curr = l->first;
+        while(curr->next != NULL){
+            curr = curr->next;
+        }
+        curr->next = malloc(sizeof(Node));
+        curr->next->val = val;
+        curr->next->next = NULL;
+    }
+    return;
+}
+
+int len(List *l){
+    Node *curr = l->first;
+    int count = 0;
+    while(curr != NULL){
+        curr = curr->next;
+        count += 1;
+    }
+    return count;
+}
+
+void print_list(List *l){
+    Node *curr = l->first;
+    printf("{");
+    while(curr != NULL){
+        if(curr->next != NULL) {
+            printf("%d, ", curr->val);
+        }
+        else{
+            printf("%d", curr->val);
+        }
+        curr = curr->next;
+    }
+    printf("}\n");
+    return;
+}
+
+int pop(List *l){
+    int val = 0;
+    Node *curr = l->first;
+    while(curr->next->next != NULL){
+        curr = curr->next;
+    }
+    val = curr->next->val;
+    free(curr->next);
+    curr->next = NULL;
+    return val;
+}
+
+void insert_at(List *l, int val, int index){
+    Node *new_node = malloc(sizeof(Node));
+    new_node->val = val;
+    new_node->next = NULL;
+    if(index == 0){
+        new_node->next = l->first;
+        l->first = new_node;
+    }
+    else{
+        Node *curr = l->first;
+        while(index != 1){
+            if(curr->next != NULL) {
+                curr = curr->next;
+            }
+            else{
+                perror("Index Error");
+                return;
+            }
+            index--;
+        }
+        new_node->next = curr->next;
+        curr->next = new_node;
+    }
+}
+
+void remove_at(List *l, int index){
+    if(len(l) <= index){
+        perror("Index Error");
+        return;
+    }
+    if(index == 0){
+        free(l->first);
+        l->first = malloc(sizeof(Node));
+        l->first = l->first->next;
+    }
+    else{
+        Node *curr = l->first;
+        while(index != 1){
+            if(curr->next != NULL){
+                curr = curr->next;
+
+            }
+            else{
+                perror("Index Error");
+                return;
+            }
+            index--;
+        }
+        free(curr->next);
+        curr->next = malloc(sizeof(Node));
+        curr->next = curr->next->next;
+    }
+}
+
+int main() {
+    List list = new_list();
+    printf("%d\n", len(&list));
+    print_list(&list);
+    insert(&list, 1);
+    insert(&list, 2);
+    insert(&list, 3);
+    insert(&list, 4);
+    print_list(&list);
+    printf("%d\n", len(&list));
+    printf("%d\n", pop(&list));
+    print_list(&list);
+    insert_at(&list, 10, 1);
+    print_list(&list);
+    remove_at(&list, 1);
+    print_list(&list);
+    remove_at(&list, 0);
+    print_list(&list);
+    remove_at(&list, 1);
+    print_list(&list);
+    remove_at(&list, 3);
+    print_list(&list);
+    return 0;
+}
+
